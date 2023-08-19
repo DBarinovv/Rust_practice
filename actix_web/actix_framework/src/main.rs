f9516bc7a2245e8e056bcecc5473be266ebd82e8
DBarinovv <9653314755@bk.ru>
2023-08-19 03:49:32 +0300

    Args pass

--- actix_web/actix_framework/src/main.rs
+++ actix_web/actix_framework/src/main.rs
@@ -4,11 +4,18 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+async fn greet_name(path: web::Path<String>) -> impl Responder {
+    format!("Hello, {}!", path)
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/greet", web::get().to(health_check))
-                    .route("/greet/{name}", web::get().to(greet_name)))
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/greet/{name}", web::get().to(greet_name))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
