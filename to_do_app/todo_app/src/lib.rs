a6e366d464c2ccdc71386d1747205c151c15bc79
DBarinovv <9653314755@bk.ru>
2023-08-27 19:42:03 +0300

    Add Task

--- to_do_app/todo_app/src/lib.rs
+++ to_do_app/todo_app/src/lib.rs
@@ -1,16 +1,33 @@
 use time::Date;
 
+#[derive(Debug)]
 enum TaskPriority {
     Low,
     Medium,
     High,
 }
 
-struct ToDo {
+struct Task {
     id: u64,
     description: String,
     priority: TaskPriority,
     due_date: Date,
+}
+
+impl Task {
+    fn print(&self) {
+        println!("Task {} with {:?} priority and due_date = {}\n ", self.id, self.priority, self.due_date);
+    }
+}
+
+struct App {
+    tasks: std::collections::BTreeSet<Task>,
+}
+
+impl App {
+    fn print_tasks(&self) -> Vec<Task> {
+        self.tasks.iter().map(|task| task.)
+    }
 }
 
 #[cfg(test)]
