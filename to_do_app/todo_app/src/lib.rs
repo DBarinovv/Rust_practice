14de700b3d9aa3df3b9f6672d4e4d7c902e86874
DBarinovv <9653314755@bk.ru>
2023-09-01 00:32:45 +0300

    Add new functions

--- to_do_app/todo_app/src/lib.rs
+++ to_do_app/todo_app/src/lib.rs
@@ -1,6 +1,9 @@
+use std::cmp::Ordering;
+use std::collections::BTreeSet;
+
 use time::Date;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum TaskPriority {
     Low,
     Medium,
@@ -11,26 +14,68 @@ struct Task {
     id: u64,
     description: String,
     priority: TaskPriority,
-    due_date: Date,
+    due_date: Option<Date>,
 }
 
-impl Task {
-    fn print(&self) {
-        println!("Task {} with {:?} priority and due_date = {}\n ", self.id, self.priority, self.due_date);
+impl Ord for Task {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.priority.cmp(&other.priority)
     }
 }
 
-struct App {
-    tasks: std::collections::BTreeSet<Task>,
+impl PartialOrd for Task {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Task {
+    fn eq(&self, other: &Self) -> bool {
+        self.priority == other.priority
+    }
+}
+
+impl Eq for Task { }
+
+impl Task {
+    pub fn new(description: String, priority: TaskPriority) -> Self {
+        Self { id: 0, description, priority, due_date: None }
+    }
+
+    fn print(&self) {
+        println!("Task {} with {:?} priority and due_date = {}\n ", self.id, self.priority, self.due_date.unwrap_or(Date::MIN));
+    }
+}
+
+pub struct App {
+    tasks: BTreeSet<Task>,
+    last_task_id: u64,
 }
 
 impl App {
-    fn print_tasks(&self) -> Vec<Task> {
-        self.tasks.iter().map(|task| task.)
+    pub fn new() -> Self {
+        Self { tasks: BTreeSet::new(), last_task_id: 1 }
+    }
+
+    fn print_tasks(&self) {
+        self.tasks.iter().map(|task| task.print());
+    }
+
+    fn add_task(&mut self, mut task: Task) {
+        task.id = self.last_task_id;
+        self.last_task_id += 1;
+
+        self.tasks.insert(task);
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn simple_test() {
+        let app = App::new();
+        // app.add_task(task);
+    }
 }
