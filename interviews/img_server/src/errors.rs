86c441ae2a18d3d32d9d04e477feddfcd26f522c
DBarinovv <9653314755@bk.ru>
2023-09-23 14:12:24 +0300

    Add docs for errors.rs

--- interviews/img_server/src/errors.rs
+++ interviews/img_server/src/errors.rs
@@ -1,6 +1,14 @@
+/// Image Processing Error Module
+/// Provides a custom error type, `ImageProcessingError`
 use std::fmt;
 use warp::reject::Reject;
 
+/// Represents various errors that can occur during the image processing
+///
+/// * `InvalidParameters(String)`: Represents errors related to invalid parameters, holds a String with a descriptive error message
+/// * `ImageReadError`: Represents errors that can occur while reading an image
+/// * `ImageConversionError`: Represents errors that can occur while converting the image, for example during color adjustments
+/// * `ImageEncodingError`: Represents errors that can occur while encoding the image, for example, converting it to a specific format
 #[derive(Debug)]
 pub enum ImageProcessingError {
     InvalidParameters(String),
